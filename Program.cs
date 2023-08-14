6f3f546f4c5c082bcca994a480576ed40d4287aa
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-08-14 14:20:43 +0530

    Welcome to UC1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,48 @@
-﻿namespace ModeAnalyzer
+﻿
+using System;
+
+class MoodAnalyser
 {
-    internal class Program
+    private string message;
+
+    public MoodAnalyser(string message)
     {
-        static void Main(string[] args)
+        this.message = message;
+    }
+
+    public string AnalyseMood()
+    {
+        try
         {
-            Console.WriteLine("Welcome to Mode Analyzer");
+            if (message.Contains("happy", StringComparison.OrdinalIgnoreCase))
+            {
+                return "Happy Mood";
+            }
+            else if (message.Contains("sad", StringComparison.OrdinalIgnoreCase))
+            {
+                return "Sad Mood";
+            }
+            else
+            {
+                throw new Exception("Mood not recognized");
+            }
         }
+        catch (Exception ex)
+        {
+            return "Error: " + ex.Message;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a message: ");
+        string inputMessage = Console.ReadLine();
+
+        MoodAnalyser moodAnalyser = new MoodAnalyser(inputMessage);
+        string mood = moodAnalyser.AnalyseMood();
+        Console.WriteLine("Mood: " + mood);
     }
 }
